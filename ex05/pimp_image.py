186322c7cedd982e994aa2b366332719edb435e6
Chan Yik Wai <86033215+Kiotto-code@users.noreply.github.com>
2023-09-24 02:17:27 +0000

    test ft_grey

--- ex05/pimp_image.py
+++ ex05/pimp_image.py
@@ -49,27 +49,63 @@ def ft_blue(array) -> np.array:
     plt.show()
 
 
+# def ft_grey(array):
+#     """make the image grey using the mean of the RGB channels"""
+#     array = array.astype(np.uint8)
+
+#     if len(array.shape) == 3 and array.shape[2] == 3:
+#         height, width, _ = array.shape
+#         grey_image = np.array([[np.mean(array[i, row])
+#                                for i in row] for row in array])
+#         # print(grey_image)
+#         # grey_image = np.array(array[:, :, :].mean)
+#         # print(grey_image)
+#         print(grey_image)
+
+#         # So, essentially, taking the mean of the RGB channels at each pixel
+#         # location collapses the color information into a single channel
+#         # representing brightness, resulting in a grayscale image where each
+#         # pixel's color is determined by its intensity.
+
+
+#         # grey_image = grey_image.reshape(height, width).astype(np.uint8)
+#         grey_image = grey_image.reshape(height, width).astype(np.uint8)
+#         plt.imshow(grey_image, cmap="gray")
+#         plt.axis('off')
+#         plt.savefig("grey_image1.png", bbox_inches='tight', pad_inches=0)
+#         plt.show()
+#         return grey_image
+#     else:
+#         raise ValueError("Input array must be a color image with three \
+#             channels (e.g., RGB).")
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
 def ft_grey(array):
-    """make the image grey using the mean of the RGB channels"""
+    """Convert a color image to grayscale using the mean of the RGB channels."""
     array = array.astype(np.uint8)
 
     if len(array.shape) == 3 and array.shape[2] == 3:
-        height, width, _ = array.shape
-        grey_image = np.array([np.mean(array[i, j])
-                               for i in range(height) for j in range(width)])
+        array[:] = array.mean(axis=2,keepdims=1)
+        grey_image = array
 
-        # So, essentially, taking the mean of the RGB channels at each pixel
-        # location collapses the color information into a single channel
-        # representing brightness, resulting in a grayscale image where each
-        # pixel's color is determined by its intensity.
-
-        grey_image = grey_image.reshape(height, width).astype(np.uint8)
-
-        plt.imshow(grey_image, cmap="gray")
+        plt.imshow(grey_image)
         plt.axis('off')
-        plt.savefig("grey_image.png", bbox_inches='tight', pad_inches=0)
+        plt.savefig("ghuh2.png", bbox_inches='tight', pad_inches=0)
         plt.show()
+        
         return grey_image
     else:
-        raise ValueError("Input array must be a color image with three \
-            channels (e.g., RGB).")
+        raise ValueError("Input array must be a color image with three channels (e.g., RGB).")
+
+
+# def ft_grey(array) -> np.array:
+#     """Convert an RGB image to grayscale."""
+#     array = np.dot(array[..., :3], [0.299, 0.587, 0.114]).astype(np.uint8)
+#     array = array[:, :, np.newaxis]
+#     print(array)
+#     array = np.repeat(array, 3, axis=2)
+#     print(array)
+#     plt.imsave("landscape_grey.jpg", array)
+#     return array
